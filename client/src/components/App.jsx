ddfbfe8742ca91c4aea5b297942d92fc0a823842
wei chen <chenwei1356@gmail.com>
2019-08-21 18:48:20 -0700

    webpack set

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        test message
+      </div>
+    )
+  }
+}
+
+export default App;
