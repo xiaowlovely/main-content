c45f2c3d774dd1348168be2af7e510a564235138
wei chen <chenwei1356@gmail.com>
2019-09-06 19:51:24 -0700

    set Dockerfile for mysql

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const db = mysql.createConnection({
-  host: '172.17.0.2',
+  host: '172.17.0.2:3306',
   user: 'root',
   password: 'student',
   database: 'nearby'
